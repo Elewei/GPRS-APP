70fb78e86b71caf7de00b17e9c5efd127620120b
qwzhang <mzhangqiwei@163.com>
2019-07-25 09:22:27 +0800

    Add TCP Serv

--- gprs/main.py
+++ gprs/main.py
@@ -1,11 +1,21 @@
 import functools
+import time, threading
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from sockserv import TCPServer
+
 
 bp = Blueprint('main', __name__)
 
 
+def startServer():
+	"""创建一个TCP服务器"""
+    serv = TCPServer()
+    serv.run_forever()
+
+
 @bp.route('/')
 def index():
+	t = threading.Thread(target=startServer, name='startServ')
     return render_template('index.html')
--- gprs/sockserv.py
+++ gprs/sockserv.py
@@ -1,68 +1,54 @@
 import socket
-import select
- 
-class SocketServer:
-    """ Simple socket server that listens to one single client. """
- 
-    def __init__(self, host = '0.0.0.0', port = 12138):
-        """ Initialize the server with a host and port to listen to. """
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.host = host
-        self.port = port
-        self.sock.bind((host, port))
-        self.sock.listen(1)
- 
-    def close(self):
-        """ Close the server socket. """
-        print('Closing server socket (host {}, port {})'.format(self.host, self.port))
-        if self.sock:
-            self.sock.close()
-            self.sock = None
- 
-    def run_server(self):
-        """ Accept and handle an incoming connection. """
-        print('Starting socket server (host {}, port {})'.format(self.host, self.port))
- 
-        client_sock, client_addr = self.sock.accept()
- 
-        print('Client {} connected'.format(client_addr))
- 
-        stop = False
-        while not stop:
-            if client_sock:
-                # Check if the client is still connected and if data is available:
-                try:
-                    rdy_read, rdy_write, sock_err = select.select([client_sock,], [], [])
-                except select.error:
-                    print('Select() failed on socket with {}'.format(client_addr))
-                    return 1
- 
-                if len(rdy_read) > 0:
-                    read_data = client_sock.recv(255)
-                    # Check if socket has been closed
-                    if len(read_data) == 0:
-                        print('{} closed the socket.'.format(client_addr))
-                        stop = True
-                    else:
-                        print('>>> Received: {}'.format(read_data.rstrip()))
-                        if read_data.rstrip() == 'quit':
-                            stop = True
-                        else:
-                            client_sock.send(read_data)
+import threading
+
+class TCPServer(object):
+    def __init__(self, host = "0.0.0.0", port = 12138):
+        """初始化对象"""
+        # 创建套接字
+        self.tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 解决程序端口占用问题
+        self.tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # 绑定本地ip地址
+        self.tcp_server_socket.bind((host, port))
+        # 将套接字变为监听套接字，最大连接数量为100
+        self.tcp_server_socket.listen(100)
+
+    def run_forever(self):
+        """设备连接"""
+        while True:
+            # 1.等待设备连接(通过ip地址和端口建立tcp连接)
+            #   如果有设备连接，则会生成用于设备和服务器通讯的套接字：new_socket
+            #   会获取到设备的ip地址和端口
+            new_socket, client_addr = self.tcp_server_socket.accept()
+            print("设备{0}已连接".format(client_addr))
+
+            # 2.创建线程处理设备的需求
+            t1 = threading.Thread(target=self.service_machine, args=(new_socket, client_addr))
+            t1.start()
+
+    def service_machine(self, new_socket, client_addr):
+        """业务处理"""
+        while True:
+            # 3.接收设备发送的数据，单次最大1024字节，按‘gbk’格式解码
+            receive_data = new_socket.recv(1024).decode("gbk")
+            # 4.如果设备发送的数据不为空
+            if receive_data:
+                # 4.1 打印接收的数据，这里可以将设备发送的数据写入到文件中
+                # 获取设备的ID信息
+                print(receive_data)
+                if receive_data[0:6] == "report":
+                    response = "SET OK:" + receive_data
+                else:
+                    receive_data = receive_data[6:].split(",")[0]
+                    # 拼接响应数据
+                    response = "alarm=" + receive_data + ",Switch:clear"
+                print(response)
+                # 4.2 返回原数据作为应答，按‘utf-8’格式编码
+                new_socket.send(response.encode("utf-8"))
+            # 5.当设备断开连接时，会收到空的字节数据，判断设备已断开连接
             else:
-                print("No client is connected, SocketServer can't receive data")
-                stop = True
- 
-        # Close socket
-        print('Closing connection with {}'.format(client_addr))
-        client_sock.close()
-        return 0
- 
-def main():
-    server = SocketServer()
-    server.run_server()
-    print 'Exiting'
- 
-if __name__ == "__main__":
-    main()
+                print('设备{0}断开连接...'.format(client_addr))
+                break
+
+        # 关闭套接字
+        new_socket.close()
