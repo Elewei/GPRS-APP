b797c4aa649e366124d1c7434af0bb7351eec511
elewei <mzhangqiwei@163.com>
2019-08-01 23:25:51 +0800

    fix bug

--- gprs/main.py
+++ gprs/main.py
@@ -15,7 +15,7 @@ device_data = {
     'location': '38.6518,104.07642',
     'tantou_wendu': '32.5',
     'jiechu_wendu' : '40.2',
-    'dianliang': '90'
+    'dianliang': '90',
     'status': 1
 }
 
